5c7854599241865997958c72fe6c6d72244c6ed4
MustapheMS <moztaphem9@gmail.com>
2023-07-24 08:49:49 +0300

    BMI-React Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import "./App.css";
+import React, { useState } from 'react';
 
-function App() {
+const BmiCalculator = () => {
+  const [name, setName] = useState('');
+  const [height, setHeight] = useState('');
+  const [mass, setMass] = useState('');
+  const [bmi, setBMI] = useState(null);
+
+  const calculateBMI = () => {
+    if (height && mass) {
+      const heightInMeters = height / 100;
+      const bmiResult = (mass / (heightInMeters * heightInMeters)).toFixed(2);
+      setBMI(bmiResult);
+    }
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <div>
+        <label>Name:</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Height (cm):</label>
+        <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} />
+      </div>
+      <div>
+        <label>Mass (kg):</label>
+        <input type="number" value={mass} onChange={(e) => setMass(e.target.value)} />
+      </div>
+      <button onClick={calculateBMI}>Calculate BMI</button>
+      {bmi !== null && (
+        <div>
+          <h3>Results:</h3>
+          <p>Name: {name}</p>
+          <p>Height: {height} cm</p>
+          <p>Mass: {mass} kg</p>
+          <p>BMI: {bmi}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default BmiCalculator;
